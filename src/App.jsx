40d014caa6a8bd79f9e8c79d4d7c5553c67fcef8
gabrielmjacques <egmoreira10@gmail.com>
2023-08-24 11:39:10 -0300

    feat: add WebSocket connection support

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef } from 'react';
+import io from 'socket.io-client';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const usernameRef = useRef();
+  const messageRef = useRef();
+
+  async function handleConnect() {
+    const socketConnection = await io.connect( 'http://localhost:3000' )
+    socketConnection.emit( 'set_username', usernameRef.current.value )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <h1>Olá</h1>
+
+        <hr />
+
+        <label htmlFor="username">Username: </label>
+        <input type="text" name='username' ref={ usernameRef } />
+        <button onClick={ () => handleConnect() }>Send</button>
+
+        <div className="chat">
+          <label htmlFor="message">Message: </label>
+          <input type="text" name='message' ref={ messageRef } />
+
+
+        </div>
+      </main>
     </>
   )
 }
